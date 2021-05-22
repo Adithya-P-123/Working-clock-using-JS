e4a55f7be3e0324038268e70993e656dad31f9b9
Adithya P <adithyapv27@gmail.com>
2021-05-22 19:04:16 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -19,7 +19,7 @@
 // hey();
 function display_c(){
 var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('display_ct()',refresh);
+mytime=setTimeout('display_ct()',refresh)
 }
 function display_ct() {
 var x = new Date()
