d2bf1059d582cd429174aa39caa3bf006dc1fd93
Adithya P <adithyapv27@gmail.com>
2021-05-26 11:06:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,24 +17,3 @@
 // }
 // hey();
 // hey();
-function display_c(){
-var refresh=1000; // Refresh rate in milli seconds
-mytime=setTimeout('display_ct()',refresh)
-}
-function display_ct() {
-var x = new Date()
-document.getElementById('ct').innerHTML = x;
-display_c();}
-setInterval(() => {
-    d = new Date(); //object of date()
-    hr = d.getHours();
-    min = d.getMinutes();
-    sec = d.getSeconds();
-    hr_rotation = 30 * hr + min / 2; //converting current time
-    min_rotation = 6 * min;
-    sec_rotation = 6 * sec;
-  
-    hour.style.transform = `rotate(${hr_rotation}deg)`;
-    minute.style.transform = `rotate(${min_rotation}deg)`;
-    second.style.transform = `rotate(${sec_rotation}deg)`;
-}, 1000);
